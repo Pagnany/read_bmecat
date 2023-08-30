a7fb4d11bd71959dc33c7f38bdb3259051ee1b1a
Pagnany <eric.schmale@gmx.de>
2023-08-30 14:44:47 +0200

    english

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ fn main() {
     for node in doc.descendants() {
         match node.tag_name().name() {
             "ARTICLE" => {
-                create_artikel(node.descendants());
+                let temp = create_article(node.descendants());
+                println!("{:?}", temp);
+
+                // to test with only one article
                 return;
             }
             _ => (),
@@ -19,135 +22,135 @@ fn main() {
     let end_time = Local::now();
     let duration = end_time.signed_duration_since(start_time);
 
-    println!("Dauer: {:?}", duration);
+    println!("Duration: {:?}", duration);
 }
 
-fn create_artikel(node: roxmltree::Descendants) {
-    let mut artikel = Artikel {
+fn create_article(node: roxmltree::Descendants) -> Article {
+    let mut article = Article {
         ..Default::default()
     };
     for descen in node {
         match descen.tag_name().name() {
             "SUPPLIER_AID" => {
-                artikel.id = descen.text().unwrap_or("").to_string();
+                article.id = descen.text().unwrap_or("").to_string();
             }
             "DESCRIPTION_SHORT" => {
-                artikel.desc_short = descen.text().unwrap_or("").to_string();
+                article.desc_short = descen.text().unwrap_or("").to_string();
             }
             "DESCRIPTION_LONG" => {
-                artikel.desc_long = descen.text().unwrap_or("").to_string();
+                article.desc_long = descen.text().unwrap_or("").to_string();
             }
             "EAN" => {
-                artikel.ean = descen.text().unwrap_or("").to_string();
+                article.ean = descen.text().unwrap_or("").to_string();
             }
             "DELIVERY_TIME" => {
-                artikel.deliver_time = descen.text().unwrap_or("").to_string();
+                article.deliver_time = descen.text().unwrap_or("").to_string();
             }
             "ARTICLE_FEATURES" => {
-                artikel
-                    .artikel_feature_groups
-                    .push(create_artikel_features(descen));
+                article
+                    .article_feature_groups
+                    .push(create_article_features(descen));
             }
             _ => (),
         }
     }
-    println!("{:?}", artikel);
+    article
 }
 
-fn create_artikel_features(descen: roxmltree::Node) -> ArtikelFeatureGroup {
-    let mut artikel_feature_group = ArtikelFeatureGroup {
+fn create_article_features(descen: roxmltree::Node) -> ArticleFeatureGroup {
+    let mut article_feature_group = ArticleFeatureGroup {
         ..Default::default()
     };
     for descen2 in descen.descendants() {
         match descen2.tag_name().name() {
             "REFERENCE_FEATURE_SYSTEM_NAME" => {
-                artikel_feature_group.sys_name = descen2.text().unwrap_or("").to_string();
+                article_feature_group.sys_name = descen2.text().unwrap_or("").to_string();
             }
             "REFERENCE_FEATURE_GROUP_ID" => {
-                artikel_feature_group.group_id = descen2.text().unwrap_or("").to_string();
+                article_feature_group.group_id = descen2.text().unwrap_or("").to_string();
             }
             "FEATURE" => {
-                let mut artikel_feature = ArtikelFeature {
+                let mut article_feature = ArticleFeature {
                     ..Default::default()
                 };
                 for descen3 in descen2.descendants() {
                     match descen3.tag_name().name() {
                         "FNAME" => {
-                            artikel_feature.name = descen3.text().unwrap_or("").to_string();
+                            article_feature.name = descen3.text().unwrap_or("").to_string();
                         }
                         "FVALUE" => {
-                            artikel_feature.value = descen3.text().unwrap_or("").to_string();
+                            article_feature.value = descen3.text().unwrap_or("").to_string();
                         }
                         "FUNIT" => {
-                            artikel_feature.unit = descen3.text().unwrap_or("").to_string();
+                            article_feature.unit = descen3.text().unwrap_or("").to_string();
                         }
                         _ => (),
                     }
                 }
-                artikel_feature_group.artikel_features.push(artikel_feature);
+                article_feature_group.article_features.push(article_feature);
             }
             _ => (),
         }
     }
-    artikel_feature_group
+    article_feature_group
 }
 
 #[derive(Debug, Clone)]
-struct Artikel {
+struct Article {
     id: String,
     desc_short: String,
     desc_long: String,
     ean: String,
     deliver_time: String,
-    artikel_feature_groups: Vec<ArtikelFeatureGroup>,
+    article_feature_groups: Vec<ArticleFeatureGroup>,
 }
 
-impl Default for Artikel {
+impl Default for Article {
     fn default() -> Self {
-        Artikel {
+        Article {
             id: "".to_string(),
             desc_short: "".to_string(),
             desc_long: "".to_string(),
             ean: "".to_string(),
             deliver_time: "".to_string(),
-            artikel_feature_groups: Vec::new(),
+            article_feature_groups: Vec::new(),
         }
     }
 }
 
 #[derive(Debug, Clone)]
-struct ArtikelPrice {
+struct ArticlePrice {
     id: String,
     price: String,
 }
 
 #[derive(Debug, Clone)]
-struct ArtikelFeatureGroup {
+struct ArticleFeatureGroup {
     sys_name: String,
     group_id: String,
-    artikel_features: Vec<ArtikelFeature>,
+    article_features: Vec<ArticleFeature>,
 }
 
-impl Default for ArtikelFeatureGroup {
+impl Default for ArticleFeatureGroup {
     fn default() -> Self {
-        ArtikelFeatureGroup {
+        ArticleFeatureGroup {
             sys_name: "".to_string(),
             group_id: "".to_string(),
-            artikel_features: Vec::new(),
+            article_features: Vec::new(),
         }
     }
 }
 
 #[derive(Debug, Clone)]
-struct ArtikelFeature {
+struct ArticleFeature {
     name: String,
     value: String,
     unit: String,
 }
 
-impl Default for ArtikelFeature {
+impl Default for ArticleFeature {
     fn default() -> Self {
-        ArtikelFeature {
+        ArticleFeature {
             name: "".to_string(),
             value: "".to_string(),
             unit: "".to_string(),
