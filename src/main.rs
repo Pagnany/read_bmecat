21407aa565056d59b925b9a29c568a72c07e35d1
Pagnany <eric.schmale@gmx.de>
2023-08-31 11:23:43 +0200

    pictures to csv

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,31 @@ mod bmecat;
 fn main() {
     let start_time = Local::now();
 
-    let temp = std::fs::read_to_string("./files/nw_bmecat.xml").unwrap();
+    let temp = std::fs::read_to_string("./files/nw_bmecat.xml").expect("Can't read file");
 
     let articles = bmecat::read_bmecat(temp);
 
-    let mut count = 0;
-
-    for article in articles {
-        println!("{}", article.id);
-        println!("{:?}", bmecat::get_article_pictures(&article));
-        count += 1;
-
-        if count > 10 {
-            break;
-        }
-    }
+    article_pictures_to_csv(articles.clone());
 
     let end_time = Local::now();
     let duration = end_time.signed_duration_since(start_time);
 
     println!("{:?}", duration);
+}
+
+fn article_pictures_to_csv(articles: Vec<bmecat::Article>) {
+    let mut wtr = csv::WriterBuilder::new()
+        .delimiter(b';')
+        .from_path("./files/article_pictures.csv")
+        .expect("Can't create file");
+
+    for article in articles {
+        let pictures = bmecat::get_article_pictures(&article);
+
+        for picture in pictures {
+            wtr.write_record(&[article.id.clone(), picture])
+                .expect("Can't write record");
+        }
+    }
+    wtr.flush().expect("Can't flush");
 }
