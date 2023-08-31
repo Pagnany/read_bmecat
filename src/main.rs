dd15b37aea4e6fe2c35edf8b9ba9f78e4fdac78a
Pagnany <eric.schmale@gmx.de>
2023-08-31 11:50:17 +0200

    article picture to csv. all pictures for article in one line.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let articles = bmecat::read_bmecat(temp);
 
-    article_pictures_to_csv(articles.clone());
+    article_picture_to_csv_one_line(articles.clone());
 
     let end_time = Local::now();
     let duration = end_time.signed_duration_since(start_time);
@@ -30,6 +30,22 @@ fn article_pictures_to_csv(articles: Vec<bmecat::Article>) {
             wtr.write_record(&[article.id.clone(), picture])
                 .expect("Can't write record");
         }
+    }
+    wtr.flush().expect("Can't flush");
+}
+
+fn article_picture_to_csv_one_line(articles: Vec<bmecat::Article>) {
+    let mut wtr = csv::WriterBuilder::new()
+        .delimiter(b';')
+        .quote_style(csv::QuoteStyle::Never)
+        .from_path("./files/article_pictures.csv")
+        .expect("Can't create file");
+
+    for article in articles {
+        let pictures = bmecat::get_article_pictures(&article);
+
+        wtr.write_record(&[article.id.clone(), pictures.join(";")])
+            .expect("Can't write record");
     }
     wtr.flush().expect("Can't flush");
 }
