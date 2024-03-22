c2ce002797dfac1abb755392f0665bdb85ed9603
Pagnany <eric.schmale@gmx.de>
2024-03-22 08:46:55 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,47 @@
 use chrono::prelude::*;
+use std::env;
 
 mod bmecat;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // Wenn der Pfad nicht übergeben wurde.
+    if args.len() != 2 {
+        println!("FEHLER: Bitte geben die den Pfad zur BMEcat Datei an.");
+
+        println!("Zum Schließen <Enter> drücken.");
+        let mut line = String::new();
+        let _ = std::io::stdin().read_line(&mut line).unwrap();
+        return;
+    }
+
     let start_time = Local::now();
 
-    let temp = std::fs::read_to_string("./files/Boschimp.xml").expect("Can't read file");
+    let file_string = std::fs::read_to_string(&args[1]).expect(
+        "FEHLER: Datei konnte nicht geöffnet werden! Die Datei muss als UTF-8 encoded sein.",
+    );
 
-    let articles = bmecat::read_bmecat(temp);
+    println!("BMEcat wird verarbeitet... Bitte haben sie etwas Gedult.");
+    let articles = bmecat::read_bmecat(file_string);
 
+    println!("CSV Datei wird generiert...");
     article_picture_to_csv_one_line(articles);
-    /*
-    articles[2..5].iter().enumerate().for_each(|(i, article)| {
-        println!("{}", i);
-        println!("{:?}", article);
-    });
-    */
 
     let end_time = Local::now();
     let duration = end_time.signed_duration_since(start_time);
 
     println!("{:?}", duration);
+
+    println!("Zum Schließen <Enter> drücken.");
+    let mut line = String::new();
+    let _ = std::io::stdin().read_line(&mut line).unwrap();
 }
 
 fn article_pictures_to_csv(articles: Vec<bmecat::Article>) {
     let mut wtr = csv::WriterBuilder::new()
         .delimiter(b';')
-        .from_path("./files/article_pictures.csv")
+        .from_path("./article_pictures.csv")
         .expect("Can't create file");
 
     for article in articles {
@@ -44,7 +59,7 @@ fn article_picture_to_csv_one_line(articles: Vec<bmecat::Article>) {
     let mut wtr = csv::WriterBuilder::new()
         .delimiter(b';')
         .quote_style(csv::QuoteStyle::Never)
-        .from_path("./files/article_pictures.csv")
+        .from_path("./article_pictures.csv")
         .expect("Can't create file");
 
     for article in articles {
